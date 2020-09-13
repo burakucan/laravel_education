b99b1734aac9ef1c87efdc6e2f80169771455a49
Naci Burak Uçan <ucannaciburak@gmail.com>
2020-09-14 00:41:06 +0300

    Page title and description added new controller created

--- resources/views/info.blade.php
+++ resources/views/info.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
-
+@section('page_description')
+@section('page_title','About')
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
@@ -11,7 +12,7 @@
                     Donec sollicitudin molestie malesuada. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi.
                     Pellentesque in ipsum id orci porta dapibus. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.
                     Cras ultricies ligula sed magna dictum porta. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi.
-                    Donec sollicitudin molestie malesuada. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. 
+                    Donec sollicitudin molestie malesuada. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.
                     Cras ultricies ligula sed magna dictum porta. Quisque velit nisi, pretium ut lacinia in, elementum id enim.
                 </div>
             </div>
--- resources/views/starting_page.blade.php
+++ resources/views/starting_page.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-
+@section('page_description')
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
