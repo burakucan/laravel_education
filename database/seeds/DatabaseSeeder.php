726d3471684c256e938bdbd5d3df141ce807540e
Naci Burak Uçan <ucannaciburak@gmail.com>
2020-09-24 20:36:34 +0300

    admin middeware created

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(TagSeeder::class);
+        $this->call(TagSeeder::class);
         $this->call(UserSeeder::class);
+        $this->call(AdminSeeder::class);
     }
 }
